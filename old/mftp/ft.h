c8cb269436759b2141f056f1f9e4039e9d281a8c
Justin R. Wilson <jrwilson@go.wustl.edu>
2011-06-30 22:08:12 -0500

    Initial import.

---
+++ old/mftp/ft.h
@@ -0,0 +1,10 @@
+#ifndef __ft_h__
+#define __ft_h__
+
+#define PORT 64470
+
+#define FILEC 0
+#define META 1
+#define QUERY 2
+
+#endif
---
+++ old/src/display.h
@@ -0,0 +1,10 @@
+#ifndef __display_h__
+#define __display_h__
+
+#include <ueioa.h>
+
+extern descriptor_t display_descriptor;
+
+void display_frame_in (void*, void*, bid_t);
+
+#endif
